661d787a69ff95ccef65dbe4d47bb90f6ead31a8
Mrinmoy Mondal <mrinmoymondalreal+github@gmail.com>
2025-08-28 17:00:26 +0530

    integrated task fetch api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,10 @@
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router";
-import TodoApp from "./routes/Page";
+import TodoApp from "./routes/tasks";
+import LoginPage from "./routes/login";
+import SignUpPage from "./routes/signup";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { fetchClient } from "./lib/fetchClient";
 
 const router = createBrowserRouter([
   {
@@ -9,13 +13,31 @@ const router = createBrowserRouter([
     lazy: async () => import("@/routes/index"),
   },
   {
-    path: "/new",
+    path: "/tasks",
     element: <TodoApp />,
+    loader: async () => {
+      const response = await fetchClient("api/tasks");
+      return await response.json();
+    },
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/signup",
+    element: <SignUpPage />,
   },
 ]);
 
+const queryClient = new QueryClient();
+
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
--- frontend/src/routes/tasks.tsx
+++ frontend/src/routes/tasks.tsx
@@ -1,41 +1,37 @@
-"use client";
-
 import { useState } from "react";
 
 import { Badge } from "@/components/ui/badge";
 import { TaskItem, type Task } from "@/components/TaskItem";
-import { Button } from "@/components/ui/button";
 import { CreateTaskButton } from "@/components/CreateTaskButton";
+import { useLoaderData } from "react-router";
 
 const TodoApp = () => {
-  const [tasks, setTasks] = useState<Task[]>([
-    {
-      id: "1",
-      title: "Add your first Project",
-      description:
-        "Tip: organizing similar or related tasks into a Project can help with the mental clutter.",
-      status: "Pending",
-      dueDate: "24 Feb 2027",
-    },
-    {
-      id: "2",
-      title: "Add Todoist to your email",
-      description: "Turn any email into a task.",
-      status: "Pending",
-    },
-    {
-      id: "3",
-      title: "Subscribe for monthly productivity inspiration",
-      description: "",
-      status: "In Progress",
-    },
-    {
-      id: "4",
-      title: "Explore our curated templates",
-      description: "",
-      status: "Completed",
-    },
-  ]);
+  const all_tasks = useLoaderData().tasks;
+  const [tasks, setTasks] = useState<Task[]>(all_tasks);
+
+  if (tasks.length === 0) {
+    return (
+      <div className="min-h-screen p-4">
+        <div className="mx-auto max-w-4xl">
+          <div className="mb-6 p-6">
+            <div className="flex items-center justify-between mb-4">
+              <div className="flex items-center gap-2">
+                <h2 className="text-lg font-medium text-gray-900 flex items-center gap-2">
+                  Task List
+                  <Badge variant="secondary" className="text-xs">
+                    0
+                  </Badge>
+                </h2>
+              </div>
+            </div>
+
+            <div className="space-y-3">No Task Avialable</div>
+          </div>
+          <CreateTaskButton tasks={tasks} setTasks={setTasks} />
+        </div>
+      </div>
+    );
+  }
 
   const completedBonusCount = tasks.filter(
     (task) => task.status === "Completed"
